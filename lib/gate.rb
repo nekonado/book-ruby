f988b35f8c5390da1e1dd376557832f2c62f40c9
queuek <lahass1213@gmail.com>
2023-10-11 09:13:30 +0900

    fix Gate and Ticket

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,2 +1,23 @@
 class Gate
+  STATIONS = [:umeda, :juso, :mikuni]
+  FARES = [160, 190]
+
+  def initialize(name)
+    @name = name
+  end
+
+  def enter(ticket)
+    ticket.stamp(@name)
+  end
+
+  def exit(ticket)
+    calc_fare(ticket) <= ticket.fare
+  end
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = (to - from).abs
+    FARES[distance - 1]
+  end
 end
--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,13 +1,34 @@
 require 'minitest/autorun'
 require_relative '../lib/gate'
+require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    assert Gate.new
-    # umeda = Gate.new(:umeda)
-    # juso = Gate.new(:juso)
-    # ticket = Ticket.new(150)
-    # umeda.enter(ticket)
-    # assert juso.exit(ticket)
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(160)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
+  end
+
+  # 順方向
+  def test_umeda_to_mikuni_when_fare_is_not_enough
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(160)
+    umeda.enter(ticket)
+    refute mikuni.exit(ticket)
+  end
+
+  # 逆方向
+  def test_mikuni_to_umeda_when_fare_is_not_enough
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(160)
+    mikuni.enter(ticket)
+    refute umeda.exit(ticket)
   end
 end
